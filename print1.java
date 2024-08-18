9b11a0c588295973bc37d825ac957b9106a34b04
Ankuristic <63995112+Ankuristic@users.noreply.github.com>
2024-08-18 14:47:18 +0530

    second commit with recursion all code

--- print1.java
+++ print1.java
@@ -2,18 +2,25 @@ package RecursionBasic;
 
 public class print1 {
 	
-	static void print () {
-		System.out.println(1);
+	static void print (int count) {
+//	int count = 0 ;
 		
-		print();
+		if (count == 4)  return ;
+		
+		
+		System.out.println(count);
+		 count = count+1;
+		
+		
+		print(count);
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		//   this is a stack overflow coondition so we provide base condition till now 
 		System.out.println(1);
 		
-		print();
+		print(0);
 
 	}
 

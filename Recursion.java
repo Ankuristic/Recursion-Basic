628f518e8531f8fbf5d2f1e941713341c2298d9e
Ankuristic <63995112+Ankuristic@users.noreply.github.com>
2024-08-18 13:51:21 +0530

    first commit through java

---
+++ Recursion.java
@@ -0,0 +1,11 @@
+package RecursionBasic;
+
+public class Recursion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         System.out.println("He is a bokachoda");
+         main(args);
+	}
+
+}
---
+++ print1.java
@@ -0,0 +1,20 @@
+package RecursionBasic;
+
+public class print1 {
+	
+	static void print () {
+		System.out.println(1);
+		
+		print();
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println(1);
+		
+		print();
+
+	}
+
+}
